3f9083b6534533cbf7738e6515be080590f82a45
unknown <lusterofgem@gmail.com>
2022-05-09 13:20:36 +0800

    add rectangle

--- src/pixelsnake/PixelSnake.java
+++ src/pixelsnake/PixelSnake.java
@@ -1,10 +1,16 @@
 package pixelsnake;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class PixelSnake extends Application
 {
+    final double width = 800;
+    final double height = 600;
+
     public static void main(String args[])
     {
         launch(PixelSnake.class, args);
@@ -14,9 +20,15 @@ public class PixelSnake extends Application
     public void start(Stage stage)
     {
         stage.setTitle("PixelSnake");
+        // stage.setMinWidth(width);
+        // stage.setMinHeight(height);
+        // stage.setMaxWidth(width);
+        // stage.setMinHeight(height);
 
-        
-        
+        Rectangle rectangle1 = new Rectangle(0, 0, 50, 50);
+        Group group = new Group(rectangle1);
+        Scene scene = new Scene(group, width, height);
+        stage.setScene(scene);
         stage.show();
 	}
 }
